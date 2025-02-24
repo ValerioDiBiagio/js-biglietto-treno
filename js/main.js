aa366d5147ca1e75bebaf4732223b932127022f8
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-02-24 19:20:11 +0100

    feat: add final price

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@ const userAge = prompt ("Quanti anni hai?");
 // Calcolare prezzo intero del bigletto
 
 const standardPrice = kmAmount * 0.21;
+
+// Calcolare il prezzo in base all'età
+
+let finalPrice;
+
+if (userAge < 18) {
+    finalPrice = ((standardPrice * 80) / 100);
+} else if (userAge > 65) {
+    finalPrice = ((standardPrice * 60) / 100);
+} else {
+    finalPrice = standardPrice;
+}
+
+console.log(finalPrice);
+console.log(finalPrice.toFixed(2));
