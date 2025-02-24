f40ffca07b0e66ab84285a962fab759eb991e75e
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-02-24 19:01:46 +0100

    feat: add standard price

--- js/main.js
+++ js/main.js
@@ -5,3 +5,7 @@ const kmAmount = prompt ("Quanti chilometri percorrerai?");
 // chiedere all'utente quanti anni ha
 
 const userAge = prompt ("Quanti anni hai?");
+
+// Calcolare prezzo intero del bigletto
+
+const standardPrice = kmAmount * 0.21;
