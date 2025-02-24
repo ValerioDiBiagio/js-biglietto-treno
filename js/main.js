5b9d66e8d43b1349e67e99df41256ad8e29df3b1
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-02-24 18:58:08 +0100

    feat: add prompt

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// chiedere all'utente quanti km percorrerà 
+
+const kmAmount = prompt ("Quanti chilometri percorrerai?");
+
+// chiedere all'utente quanti anni ha
+
+const userAge = prompt ("Quanti anni hai?");
