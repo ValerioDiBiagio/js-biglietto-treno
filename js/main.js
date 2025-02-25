b395d0a30ee169468eed0e417a765d2a0b05d229
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-02-25 13:28:14 +0100

    feat: add parse

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // chiedere all'utente quanti km percorrerà 
 
-const kmAmount = prompt ("Quanti chilometri percorrerai?");
+const kmAmount = parseFloat(prompt ("Quanti chilometri percorrerai?"));
 
 // chiedere all'utente quanti anni ha
 
-const userAge = prompt ("Quanti anni hai?");
+const userAge = parseInt(prompt ("Quanti anni hai?"));
 
 // Calcolare prezzo intero del bigletto
 
